dbf5bfe3b7144508c05221541999207aba6d0f25
rlobz <rlobzowski@gmail.com>
2023-12-13 16:59:23 +0000

    Middleware for parsing JSON and urlencoded form data

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
