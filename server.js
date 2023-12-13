e17ca480df6cb2ed8bf9d1de3144d6b9f6ea91d6
rlobz <rlobzowski@gmail.com>
2023-12-13 17:01:29 +0000

    get route for homepage

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
